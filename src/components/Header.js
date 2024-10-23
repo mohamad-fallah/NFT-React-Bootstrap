9341fc7f7a2dd61c92e63b27d1cc07944c90e0c3
Mohammadreza Fallahfaal <mfallahfaal@gmail.com>
2024-10-23 11:47:04 +0330

    fix bug and structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <>
+      <header>
+        <nav>
+          سبلم
+        </nav>
+      </header>
+    </>
+  );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <h1>سلام خوبی</h1>
+    <>
+        
+    </>
   )
 }
